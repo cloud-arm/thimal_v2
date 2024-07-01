bc184604d83a37a06bdaa2800a997bc1e86028b3
ashen <hansakagaa@gmail.com>
2024-07-01 10:54:41 +0530

    up

--- testing/product.php
+++ testing/product.php
@@ -1,4 +1,7 @@
 <?php
+include('../connect.php');
+
+
 $api_url = 'https://thimal.cloudarmsoft.com/main/pages/v2/api/product.php';
 
 $curl = curl_init($api_url);
@@ -10,12 +13,31 @@ $data = json_decode($response, true);
 
 // Display the fetched data
 if (is_array($data)) {
-    echo "<h1>Product List</h1><ul>";
+    echo "<h1>Product List for API</h1><ul>";
     foreach ($data as $product) {
-        echo "<li>{$product['name']} - \${$product['price']} (Quantity: {$product['qty']})</li>";
+        echo "<li>[{$product['id']}]{$product['name']} - Rs.{$product['price']} (Quantity: {$product['qty']})</li>";
     }
     echo "</ul>";
 } else {
     echo "Failed to fetch product data.";
 }
-?>
+
+echo "<br><br><br>";
+
+echo "<h1>Product List for Datatable</h1><ul>";
+
+$result = $db->prepare("SELECT * FROM products ");
+$result->bindParam(':userid', $res);
+$result->execute();
+for ($i = 0; $product = $result->fetch(); $i++) {
+    echo "<li>[{$product['id']}]{$product['name']} - Rs.{$product['price']} (Quantity: {$product['qty']})</li>";
+}
+
+echo "</ul>";
+
+
+if ($data == $result) {
+    echo "Testing successfull..!";
+} else {
+    echo "Testing faild..!";
+}
