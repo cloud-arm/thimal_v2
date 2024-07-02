cf2d16b66660b6171b86756def9e5a9b28ba66dd
ashen <hansakagaa@gmail.com>
2024-07-02 11:06:57 +0530

    up api

--- api/sync/set_gps.php
+++ api/sync/set_gps.php
@@ -1,5 +1,6 @@
 <?php
 include('../../connect.php');
+include('../../config.php');
 date_default_timezone_set("Asia/Colombo");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -9,49 +10,60 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $date = date("Y-m-d");
 $time = date('H:i:s');
 
+$id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
+$lat = isset($_POST['lat']) ? $_POST['lat'] : null;
+$lng = isset($_POST['lng']) ? $_POST['lng'] : null;
+
+if (!$id || !$lat || !$lng) {
+    // create error respond 
+    $result_array[] = array(
+        "status" => "failed",
+        "message" => "Error: Missing parameters",
+        "action" => 0,
+    );
+
+    echo (json_encode($result_array));
+    exit;
+}
+
 // respond init
 $result_array = array();
 
-$id = $_POST['user_id'];
-$lat = $_POST['lat'];
-$lng = $_POST['lng'];
-$load_id=0;
-
+$load_id = 0;
 $result = $db->prepare("SELECT * FROM loading WHERE  driver ='$id' AND action='load' ");
 $result->bindParam(':id', $res);
 $result->execute();
-for($i=0; $row = $result->fetch(); $i++){ 
-    $load_id=$row['transaction_id'];
- }
+for ($i = 0; $row = $result->fetch(); $i++) {
+    $load_id = $row['transaction_id'];
+}
 
 
 
 //------------------------------------------------------------------//
 try {
 
-    // unloading
-    $sql = "UPDATE user SET   lat = ?, lng = ?, date = ?, time = ?  WHERE EmployeeId = ? ";
-    $q = $db->prepare($sql);
-    $q->execute(array($lat, $lng, $date, $time, $id));
-
-    // create success respond 
-    $res = array(
-        "status" => "success",
-        "message" => "",
-        "action" => $load_id,
+    $updateData = array(
+        "lat" => $lat,
+        "lng" => $lng,
+        "date" => $date,
+        "time" => $time,
     );
 
-    array_push($result_array, $res);
+    $status =  update("user", $updateData, "EmployeeId = " . $id, "../../");
+
+    $result_array[] = array(
+        "status" => $status['status'],
+        "message" => $status['message'],
+        "action" => $load_id,
+    );
 } catch (PDOException $e) {
 
     // create error respond 
-    $res = array(
+    $result_array[] = array(
         "status" => "failed",
         "message" => $e->getMessage(),
         "action" => $load_id,
     );
-
-    array_push($result_array, $res);
 }
 
 
