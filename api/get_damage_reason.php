ce4733e6c865a325bf8e818c797df7d7c9a26449
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-07-25 06:20:58 +0530

    update

--- api/get_damage_reason.php
+++ api/get_damage_reason.php
@@ -6,20 +6,19 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+// respond init
+$result_array = [];
 
-try {
-    // Prepare and execute the SQL query
-    $result = $db->prepare("SELECT * FROM damage_reason ");
-    $result->execute();
-
-    // Fetch the results and create an array
-    $result_array = array();
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $result_array[] = $row;
-    }
-
-    // Encode the array into JSON and output it
-    echo json_encode($result_array);
-} catch (PDOException $e) {
-    echo json_encode(array("message" => "Error: " . $e->getMessage()));
+// create success respond 
+$result = $db->prepare("SELECT * FROM damage_reason ");
+$result->bindParam(':id', $res);
+$result->execute();
+for ($i = 0; $row = $result->fetch(); $i++) {
+    $result_array[] = array(
+        "id" => $row['id'],
+        "name" => $row['name']
+    );
 }
+
+// send respond
+echo (json_encode($result_array));
